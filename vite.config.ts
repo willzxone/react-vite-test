9d0830db8e6f6964382af27ddd86a033991a9fd7
willzxone <willzxone0987@gmail.com>
2024-02-06 16:46:56 +0500

    Runtime caching added

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,19 @@ export default defineConfig({
     VitePWA({
       workbox: {
         globPatterns: ["**/*"],
+        runtimeCaching: [{
+          handler: 'NetworkOnly',
+          urlPattern: /\/api\/.*\/*.json/,
+          method: 'POST',
+          options: {
+            backgroundSync: {
+              name: 'myQueueName',
+              options: {
+                maxRetentionTime: 24 * 60
+              }
+            }
+          }
+        }]
       },
       includeAssets: [
         "**/*",
