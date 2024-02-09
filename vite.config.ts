efcd89a071e31779de662f5cbcb0a177d515ab92
willzxone <willzxone0987@gmail.com>
2024-02-10 00:03:42 +0500

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
-import basicSsl from '@vitejs/plugin-basic-ssl'
-import fs from 'fs'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
